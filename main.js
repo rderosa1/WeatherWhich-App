14678826f24c6b3f75e236f9e663ad9229b5959c
Ricardo DeRosa <rico@Ricardos-MacBook-Pro.local>
2020-02-05 15:47:12 -0500

    added forecast button and information

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q="
+BASE_URL2 = "http://api.openweathermap.org/data/2.5/forecast?q="
 API_KEY = "APPID=af8425eab3b3aa4292a1f637f44d97de"
 const button = document.querySelector(".press")
 const cityOption = document.querySelector("input")
@@ -6,6 +7,13 @@ const weatherDiv =  document.querySelector(".weatherData")
 const moreButton = document.querySelector(".moreButton")
 const hiddenDiv = document.querySelector(".hidden")
 const main = document.querySelector("main")
+const forecastDiv = document.querySelector(".forecastDiv")
+const forecastData = document.querySelector(".forecastData")
+const forecastBtn = document.querySelector(".forecastBtn")
+const fcDiv1 = document.querySelector(".fcDiv1")
+const fcDiv2 = document.querySelector(".fcDiv2")
+const fcDiv3 = document.querySelector(".fcDiv3")
+
 const cityArray = ["new york", "london", "paris", "hong kong", "guadalajara", "los angeles", "tokyo", "bangkok", "dubai", "singapore"]
 
 button.addEventListener("click", async () => {
@@ -13,7 +21,7 @@ button.addEventListener("click", async () => {
     let cityName = cityOption.value
 
     let response = await axios.get(`${BASE_URL}${cityName}&${API_KEY}&units=imperial`)
-    console.log(response.data)
+    // console.log(response)
 
 
     let condition = response.data.weather[0].description
@@ -25,7 +33,29 @@ button.addEventListener("click", async () => {
     weatherDiv.innerHTML = `<p>Currently: ${condition} at ${temp}° F </p> <br> <p> Feels like: ${feelsLike}° F</p>` 
     
     hiddenDiv.innerHTML = `<p>Daily Minimum Temp: ${minTemp}° F</p> <br> <p>Daily Maximum Temp: ${maxTemp}° F</p>`
+
     
+    let forecast = await axios.get(`${BASE_URL2}${cityName}&${API_KEY}&units=metric`)
+    // console.log(forecast.data.list)
+
+    let castDay1TempMin = forecast.data.list[0].main.temp_min
+    let castDay1TempMax = forecast.data.list[0].main.temp_max
+    let castDay1condition = forecast.data.list[0].weather[0].description
+    let castDay2TempMin = forecast.data.list[1].main.temp_min
+    let castDay2TempMax = forecast.data.list[1].main.temp_max
+    let castDay2condition = forecast.data.list[1].weather[0].description
+    let castDay3TempMin = forecast.data.list[2].main.temp_min
+    let castDay3TempMax = forecast.data.list[2].main.temp_max
+    let castDay3condition = forecast.data.list[2].weather[0].description
+
+    // console.log(castDay2TempMax, castDay3TempMax, castDay2condition, castDay3condition)
+
+        fcDiv1.innerHTML = `<h2> Day 1 </h2> <p>Expect ${castDay1condition} with a range of temperature from ${castDay1TempMin}°C to ${castDay1TempMax}°C </p>`
+        fcDiv2.innerHTML = `<h2> Day 2 </h2> <p> Expect ${castDay2condition} with a range of temperature from ${castDay2TempMin}°C to ${castDay2TempMax}°C</p>`
+        fcDiv3.innerHTML = ` <h2> Day 3 </h2> <p>Expect ${castDay3condition} with a range of temperature from ${castDay3TempMin}°C to ${castDay3TempMax}°C</p>`
+
+
+
 
 })
 
@@ -69,3 +99,10 @@ moreButton.addEventListener("click", () => {
 
 })
 
+forecastBtn.addEventListener("click", () => {
+    if (forecastData.style.display == "none"){
+    forecastData.style.display = "flex"
+    } else {
+        forecastData.style.display = "none"
+    }
+})
